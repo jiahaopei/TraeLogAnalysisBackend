6acb7e17814612a6dad310281539d8788b08c824
jiahaopei <353821299@qq.com>
2025-12-31 17:08:18 +0800

    Fix SQLite dialect by simplifying to minimal compatible implementation for Hibernate 6.x

--- src/main/java/com/trae/loganalysis/config/SQLiteDialect.java
+++ src/main/java/com/trae/loganalysis/config/SQLiteDialect.java
@@ -1,13 +1,11 @@
 package com.trae.loganalysis.config;
 
-import org.hibernate.dialect.pagination.LimitOffsetLimitHandler;
-import org.hibernate.dialect.pagination.LimitHandler;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.identity.IdentityColumnSupport;
 import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
 
 /**
- * SQLite方言实现，支持Hibernate 6.x的分页
+ * SQLite方言实现，兼容Hibernate 6.x
  */
 public class SQLiteDialect extends Dialect {
 
@@ -34,51 +32,5 @@ public class SQLiteDialect extends Dialect {
                 return "primary key autoincrement";
             }
         };
-    }
-
-    @Override
-    public boolean hasAlterTable() {
-        return false;
-    }
-
-    @Override
-    public boolean dropConstraints() {
-        return false;
-    }
-
-    @Override
-    public String getDropForeignKeyString() {
-        return "";
-    }
-
-    @Override
-    public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable, String[] primaryKey, boolean referencesPrimaryKey) {
-        return "";
-    }
-
-    @Override
-    public String getAddPrimaryKeyConstraintString(String constraintName) {
-        return "";
-    }
-
-    @Override
-    public String getForUpdateString() {
-        return "";
-    }
-
-    @Override
-    public boolean supportsOuterJoinForUpdate() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsIfExistsBeforeTableName() {
-        return true;
-    }
-    
-    @Override
-    public LimitHandler getLimitHandler() {
-        // Use the built-in LimitOffsetLimitHandler which generates LIMIT ? OFFSET ? syntax
-        return LimitOffsetLimitHandler.INSTANCE;
     }
 }
